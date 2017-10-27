2b97070f461b1ac345e6296fac235e8b909b3ce2
KaitieFox <kaitiefox@gmail.com>
2017-10-27 10:31:53 -0400

    Added List

--- ToDo/Models/Item.cs
+++ ToDo/Models/Item.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -15,7 +16,9 @@ namespace ToDo.Models
         public string Details { get; set; }
         public bool IsDone { get; set; }
 
-
+        [ForeignKey("List")] //references the class we are linking with the foreignkey
+        public int ListID { get; set; }
+        public virtual List List { get; set; }
 
     }
 }
